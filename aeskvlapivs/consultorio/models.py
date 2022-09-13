f193bf7e3bde3ce7d17746c280a14c8a052e27f1
juanvictor <Victor@MacBook-Pro-de-Victor.local>
2022-09-12 22:58:31 -0600

    Evaluaciones secundarias de urgencias, fase inicial

--- aeskvlapivs/consultorio/models.py
+++ aeskvlapivs/consultorio/models.py
@@ -446,6 +446,9 @@ class Reevaluacion(models.Model):
         verbose_name_plural='b) Reevaluación'
 
 
+#URGENCIAS
+
+
 
 class Urgencias(models.Model):
     nombre = models.ForeignKey(Paciente, on_delete=models.CASCADE)
@@ -537,10 +540,6 @@ class Urgencias(models.Model):
 
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name='Fecha de Registro')
     update = models.DateTimeField(auto_now=True, verbose_name='Fecha de Actualización')
-
-    
-
-
 
 
     @property
@@ -662,6 +661,195 @@ class Urgencias(models.Model):
         ordering = ('-timestamp',)       
         verbose_name_plural='c) Urgencias, Evaluación Primaria'
         
+
+
+
+
+
+
+# REEVALUACIONES DE URGENCIAS
+
+class Urgencias_Reevaluaciones(models.Model):
+    ev_1 = 'Primera Evaluación'
+    ev_2 = 'Segunda Evaluación'
+    ev_3 = 'Tercera Evaluación'
+    ev_4 = 'Cuarta Evaluación'
+    ev_5 = 'Quinta Evaluación'
+    ev_6 = 'Sexta Evaluación'
+    ev_7 = 'Séptima Evaluación'
+    ev_8 = 'Octava Evaluación'
+    REEV_SECS = [(ev_1, 'Primera Evaluación'), (ev_2, 'Segunda Evaluación'), (ev_3, 'Tercera Evalaución'),
+    (ev_4, 'Cuarta Evaluación'), (ev_5, 'Quinta Evaluación'), (ev_6, 'Sexta Evaluación'), (ev_7, 'Séptima Evaluación'),
+    (ev_8, 'Octava Evaluación')]
+
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    edad = models.SmallIntegerField(default=0)
+    evaluacion_sec = models.CharField(max_length=50, choices=REEV_SECS, blank=True, null=True, verbose_name='Evaluación Subsecuente')
+    dxs_previos = models.TextField(blank=True, null=True)
+    sit_actual = models.TextField(blank=True, null=True, verbose_name='Situación Actual')
+
+# REEXPLORACION FISICA
+
+    
+
+    POSITIVO = 'POS'
+    NEGATIVO = 'NEG' 
+    AFIRMACION_SIMPLE = [ (POSITIVO, 'Positivo'), (NEGATIVO, 'Negativo')]
+    Diaforesis = models.CharField(max_length=100,choices=AFIRMACION_SIMPLE, blank=True, null=True )
+
+    fr= models.IntegerField(default=0, blank=True, null=True, verbose_name='FR')
+    O2 = models.PositiveSmallIntegerField(default=0, blank=True, null=True, help_text='En litros/min')
+    FiO2 = models.DecimalField(blank=True, null=True, max_digits=5, decimal_places=2, default=.21, help_text= 'No escriba aquí')
+    saturacion = models.IntegerField(blank=True, null=True, verbose_name='Sa02', default=1)
+    SpFI = models.PositiveSmallIntegerField(blank=True, null=True, help_text='No escriba aquí')
+
+    @property
+    def spfi(self):
+        return self.saturacion / self.FiO2
+
+        #unsupported operand type(s) for /: 'NoneType' and 'float'
+
+    @property
+    def sira(self):
+        if self.SpFI < 419 and self.fr > 20:
+            return 'Disfunción Respiratoria, Realice Gasometría'
+    
+    fc = models.IntegerField(default=0, blank=True, null=True, verbose_name='FC')
+    tension_sistolica = models.PositiveSmallIntegerField(blank=True, null=True, default=0)
+    tension_diastolica = models.PositiveSmallIntegerField(blank=True, null=True, default=0)
+    pam = models.IntegerField(default=0, verbose_name='PAM', help_text='No escriba aquí')
+
+    @property
+    def presion_media(self):
+        return (self.tension_sistolica + (2 * self.tension_diastolica))/3 
+
+    @property
+    def shock(self):
+        if self.pam < 70 and self.fc >95 and self.Diaforesis == 'POS' :
+            return 'Descarte Estado de Choque o asegúrese de haber checado TA y FC '
+
+        else:
+            return ''
+
+    @property
+    def hta(self):
+        if self.tension_diastolica >90 and self.tension_diastolica< 104:
+            return 'Hipertensión Leve ' 
+
+        if self.tension_diastolica > 104 and self.tension_diastolica< 114:
+            return 'Hipertensión Moderada, '
+
+        if self.tension_diastolica > 114:
+            return 'Hipertensión Severa / Descarte Crisis Hipertensiva '
+
+        else:
+            return ''
+
+    dextrostix = models.IntegerField(blank=True, null=True, verbose_name='Dextrostix', default=0)
+    temp =  models.DecimalField(blank=True, null=True, max_digits=5, decimal_places=2, verbose_name='Temp')
+    peso = models.FloatField(blank=True, null=True, default=0, help_text='Si no puede pesar al paciente, calcule un aproximado') 
+    estatura = models.FloatField(blank=True, null=True, default=1, help_text='En metros y cms')
+    imc = models.DecimalField(max_digits=5, decimal_places=2, default=0, verbose_name='IMC', help_text='No escriba aquí')
+    climc = models.CharField(max_length=100, blank=True, null=True, verbose_name='Clasificación Masa Corporal', help_text='No escriba aquí')
+    asc = models.DecimalField(max_digits=5, decimal_places=2, default=0, verbose_name='ASC', help_text='No escriba aquí')
+    per_abdominal = models.IntegerField(blank=True, null=True, verbose_name='Per. Abd en cms')
+
+    @property
+    def peso_urg1(Urgencias):
+        return Urgencias.peso
+
+    @property
+    def estatura_urg1(Urgencias):
+        return Urgencias.estatura
+
+    @property
+    def imc_urg1(Urgencias):
+        return Urgencias.imc
+
+    @property
+    def climc_urg1(Urgencias):
+        return Urgencias.climc
+    
+    @property
+    def asc_urg1(Urgencias):
+        return Urgencias.asc
+
+    @property
+    def fio2(self):
+        return (3 * self.O2 + 21) / 100
+
+
+    A = 'Alerta'
+    V = 'Respuesta Verbal'
+    P = 'Respuesta al Dolor'
+    U = 'Sin Respuesta'
+    CONC = [(A, 'A Alerta'), (V, 'V Respuesta Verbal'), (P, 'P Respuesta al Dolor'),
+    (U, 'U Sin Respuesta'),]
+    AVPU =  models.CharField(max_length=50, choices=CONC, verbose_name='Estado de Conciencia', blank=True, null=True)
+
+    #PARAMETROS DE GLASGOW
+    Espontanea = 4
+    Orden_verbal = 3
+    Est_doloroso = 2
+    No_apertura = 1
+    AP_OCULAR = [(Espontanea, 4 ), (Orden_verbal, 3), (Est_doloroso, 2), (No_apertura, 1)]
+    apertura_ocular = models.PositiveSmallIntegerField(choices=AP_OCULAR, blank=True, null=True, default=1, help_text='Espontánea 4; Orden verbal 3; Est. Doloroso 2; No Apertura 1.')
+
+    Orientado = 5
+    Desorientado_Confuso = 4
+    Incoherente = 3
+    Sonidos_incomprensibles = 2
+    Ausencia_Respuesta = 1
+    RESP_VERBAL = [(Orientado, 5), (Desorientado_Confuso, 4), (Incoherente, 3), (Sonidos_incomprensibles, 2), 
+    (Ausencia_Respuesta, 1),]
+    Respuesta_Verbal = models.PositiveSmallIntegerField(blank=True, null=True, default=1, choices=RESP_VERBAL, help_text='Orientado 5; Desorientado confuso 4; Incoherente 3; Sonidos incomprensibles 1.')
+
+    Obedece_ordenes = 6
+    Localiza_dolor = 5
+    Retirada_al_dolor = 4
+    Flexion_anormal = 3
+    Extension_anormal = 2
+    Ausencia_respuesta = 1
+    RESP_MOTORA = [(Obedece_ordenes, 6), (Localiza_dolor, 5), (Retirada_al_dolor, 4), (Flexion_anormal, 3),
+    (Extension_anormal, 2), (Ausencia_respuesta, 1),]
+    Respuesta_Motora = models.PositiveSmallIntegerField(choices=RESP_MOTORA, default=1, blank=True, null=True, help_text='Obedece órdenes 6; Localiza dolor 5; Retirada al dolor 4; Flexión anormal 3; Extensión anormal 2; Ausencia de Respuesta 1.')
+
+    ESCALA_DE_GLASGOW = models.PositiveSmallIntegerField(blank=True, null=True, help_text='sume los parámetros')
+
+    @property
+    def glasgow(self):
+        return self.apertura_ocular + self.Respuesta_Verbal + self.Respuesta_Motora
+
+    @property
+    def consid(self):
+        if self.ESCALA_DE_GLASGOW < 9:
+            return 'Considere Manejo Avanzado de Vía Aérea y/o Reevalúe Glasgow'
+
+    @property
+    def qsofa(self):
+        if self.tension_sistolica < 100 and self.ESCALA_DE_GLASGOW < 15 and self.fr >22:
+            return 'Si paciente no es de Trauma: qSOFA indica que hay que descartar Sepsis'
+
+
+
+
+
+    def save(self):
+        self.peso = self.estatura_urg1
+        self.estatura =self.estatura_urg1
+        self.pam = self.presion_media
+        self.FiO2 = self.fio2
+        self.SpFI = self.spfi
+        self.imc = self.imc_urg1
+        self.asc = self.asc_urg1
+        self.climc = self.imc_urg1
+        self.ESCALA_DE_GLASGOW = self.glasgow
+        self.NEUROLOGICO = self.consid
+        self.RESPIRATORIO = self.sira
+        self.HEMODINAMICO = self.shock, self.hta
+        self.INFECCIOSO = self.qsofa
+        super(Urgencias_Reevaluaciones, self).save()
+
     
             
 
