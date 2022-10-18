068a4ede8214320c4dd6db020e2da3c2466e332c
juanvictor <juanvictoremilio@icloud.com>
2022-10-18 17:46:16 -0600

    config settings directory

--- aeskvlapivs/aeskvlapivs/settings/base.py
+++ aeskvlapivs/aeskvlapivs/settings/base.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -58,7 +57,7 @@ ROOT_URLCONF = 'aeskvlapivs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR /'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,10 +79,9 @@ WSGI_APPLICATION = 'aeskvlapivs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
---
+++ aeskvlapivs/aeskvlapivs/settings/pro.py
@@ -0,0 +1,11 @@
+from .base import *
+DEBUG = False
+ADMINS = (
+    ('Víctor Sánchez', 'direccion@amcare.com-mx'),
+)
+ALLOWED_HOSTS = ['*']
+DATABASES = {
+    'default': {
+        }
+}
+
