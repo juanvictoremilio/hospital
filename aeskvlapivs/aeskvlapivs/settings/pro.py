332200540b31b52b749172e40c170e53eb0070d5
juanvictor <juanvictoremilio@icloud.com>
2022-10-20 15:05:53 -0600

    Se agrega postgresql a settings.pro

--- aeskvlapivs/aeskvlapivs/settings/pro.py
+++ aeskvlapivs/aeskvlapivs/settings/pro.py
@@ -1,11 +1,17 @@
 from .base import *
-DEBUG = False
+DEBUG = True
 ADMINS = (
     ('Víctor Sánchez', 'direccion@amcare.com-mx'),
 )
 ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aeskvlapivs',
+        'USER': 'postgres',
+        'PASSWORD': 'ayla122333',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
         }
 }
 
